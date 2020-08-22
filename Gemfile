8d5691807e83d518eb866153c844bb6a12409c25
Matias <matias.acevedoc@gmail.com>
2020-08-22 17:25:21 -0400

    commit 1

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.0'
+ruby '2.7.1'
+gem 'msgpack', '~> 1.3', '>= 1.3.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "users/login"
   get  "pages/about_us"
+  get  "pages/test"
+  get "pages/index"
+  resources :pages
   root "pages#index"
 end
